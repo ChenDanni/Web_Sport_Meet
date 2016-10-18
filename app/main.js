7b7d9ae8e22e2dbe3c257f4251f8c897e439b6f4
ChenDanni <danniChen96@163.com>
2016-10-18 19:12:34 +0800

    Build up

---
+++ app/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by chendanninode_modules/.bin/webpack app/main.js public/bundle.js on 16/10/18.
+ */
+import React from 'react';
+import {render} from 'react-dom';
+import Greeter from './Greeter';
+
+render(<Greeter />, document.getElementById('root'));
